074836a29dd2c11ac9e281f8e644c6b50d2f2629
Jaedan <jaedanparsons@gmail.com>
2024-08-01 08:07:02 -0800

    Day 37: Divided the temp to its actual value: they are given like 289 but are supposed to be 28.9
    Also showed error message if they put in a place not available

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"Temperature for the next {days} days in {place}")
 
-if place:
-    # Get the temperature/sky data
-    filtered_data = get_data(place, days)
+try:
+    if place:
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
 
-    if option == "Temperature":
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        # Create a temperature plot
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] for dict in filtered_data]
+            temperatures = [each/10 for each in temperatures]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-    else: # option = Sky
-        weather_forecast = [dict["weather"][0]["main"] for dict in filtered_data]
-        for weather in weather_forecast:
-            st.image(f"images/{str(weather).lower()}.png", width=115)
+        else: # option = Sky
+            weather_forecast = [dict["weather"][0]["main"] for dict in filtered_data]
+            for weather in weather_forecast:
+                st.image(f"images/{str(weather).lower()}.png", width=115)
+except KeyError:
+    st.write(f"Our apologies but we couldn't find {place} in our database")
